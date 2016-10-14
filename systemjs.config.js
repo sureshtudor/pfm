db4606e8bf4a69488be255ec4fce72bac4ff0296
Tudor <studor@corelogic.com>
2016-10-13 22:54:03 -0700

    adding ng2-bootstrap library.

--- systemjs.config.js
+++ systemjs.config.js
@@ -13,18 +13,21 @@
       // our app is within the app folder
       app: 'app',
       // angular bundles
-      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
-      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+      '@angular/core':                'npm:@angular/core/bundles/core.umd.js',
+      '@angular/common':              'npm:@angular/common/bundles/common.umd.js',
+      '@angular/compiler':            'npm:@angular/compiler/bundles/compiler.umd.js',
+      '@angular/platform-browser':    'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
       '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
-      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      '@angular/http':                'npm:@angular/http/bundles/http.umd.js',
+      '@angular/router':              'npm:@angular/router/bundles/router.umd.js',
+      '@angular/forms':               'npm:@angular/forms/bundles/forms.umd.js',
       // other libraries
-      'rxjs':                      'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
-      'underscore':                'npm:underscore/underscore.js'
+      'rxjs':                         'npm:rxjs',
+      'angular-in-memory-web-api':    'npm:angular-in-memory-web-api',
+      'underscore':                   'npm:underscore/underscore.js',
+      // ng2-bootstrap 
+      'ng2-bootstrap/ng2-bootstrap':  'npm:ng2-bootstrap/bundles/ng2-bootstrap.umd.js',
+      'moment':                       'npm:moment/moment.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
