f311af0117d55097fcefb00467c194a81ac71fed
sureshtudor <studor@corelogic.com>
2016-10-27 22:12:12 -0700

    changes to shared\segment component.

--- app/shared/segment/segment.component.ts
+++ app/shared/segment/segment.component.ts
@@ -1,3 +1,30 @@
-/**
- * Created by studor on 10/27/2016.
- */
+import {Component, Input}       from '@angular/core';
+import {FormGroup, FormBuilder} from "@angular/forms";
+
+@Component({
+    selector: 'segment',
+    templateUrl: 'app/shared/segment/segment.component.html'
+})
+export class SegmentComponent {
+
+    @Input() public form: FormGroup;
+
+    @Input() public hideRemark: boolean;
+
+    @Input() public hideChargeable: boolean;
+
+    constructor(private _fb: FormBuilder) {
+
+        this.form = this._fb.group({
+            overridden: false,
+            modified: false,
+            supplement: false,
+            verified: false,
+            deleted: false,
+            printable: false,
+            chargeable: false,
+            stauts: 4,
+            remark: ''
+        });
+    }
+}
---
+++ app/shared/segment/segment.ts
@@ -0,0 +1,19 @@
+export interface ISegment {
+    overridden: boolean;
+    modified: boolean;
+    supplement: boolean;
+    verified: boolean;
+    deleted: boolean;
+    printable: boolean;
+    chargeable?: boolean;
+    stauts?: number;
+    remark?: string;
+}
+
+export const STATUS_TYPE = [
+    {value: 0, name: "Fix Edit"},
+    {value: 1, name: "Review Edit"},
+    {value: 2, name: "Manual Edit"},
+    {value: 3, name: "Supplemented"},
+    {value: 4, name: "None Pending"}
+];
